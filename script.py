e3f3c3efaffc58780b2eb36336e215d534990249
Raul <rauleulogio3@gmail.com>
2016-09-17 01:05:43 -0700

    Update script.py

--- script.py
+++ script.py
@@ -30,9 +30,6 @@
 # statsmodel
 
 import pandas as pd
-import plotly 
-from plotly.graph_objs import *
-import plotly.plotly as py 
 import numpy as np 
 import statsmodels.api as sm  
 from statsmodels.tsa.arima_model import ARIMA
@@ -40,8 +37,6 @@ from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import matplotlib.pylab as plt 
 from matplotlib.pylab import rcParams
-plotly.tools.set_credentials_file(username = 'user', api_key = 'password')
-
 
 
 ##
@@ -51,11 +46,12 @@ plotly.tools.set_credentials_file(username = 'user', api_key = 'password')
 ######
 ####
 ##
-# fig, ax = plt.subplots( nrows=1, ncols=1)
-# print data.head()
+# 
+# 
 
 
-
+# Recall that in order for the terminal to output txt write 
+# python script.py > output.txt
 
 dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m')
 data = pd.read_csv('sp_500_ts.csv', parse_dates='Date', index_col='Date', date_parser=dateparse)
@@ -70,9 +66,9 @@ print data.head()
 ####
 ##
 
-ts = data['sp_500'] 
+sp_500 = data['sp_500'] 
 
-print ts
+print sp_500
 
 ##
 ####
@@ -84,18 +80,18 @@ print ts
 
 
 # Plotting the time series object 
-plt.plot(ts)
+plt.plot(sp_500)
 plt.title('Time Series Plot of S&P 500')
 plt.savefig("timeSeries.png", format = 'png')
 plt.close()
 
 # Plotting the time series object after being differenced
-ts_diff = ts - ts.shift()
-ts_diff = ts_diff.dropna()
-print "This is the one with Pandas!"
-print ts_diff
+sp500_diff = sp_500 - sp_500.shift()
+diff = sp500_diff.dropna()
+print
+print diff
 
-plt.plot(ts_diff)
+plt.plot(diff)
 plt.title('First Difference Time Series Plot')
 plt.savefig("timeSeriesDiff.png", format = 'png')
 plt.close()
@@ -103,21 +99,23 @@ plt.close()
 
 
 
-# Test try of plot_acf
-acf = plot_acf(ts, lags = 20)
+# We grab the acf and pacf for the time series with plot_acf and plot_pacf
+# NOTE: Since I am in th process of getting acquainted I know that plot_acf outputs 'fig'
+# this means I can run the same functionalities as plt!! 
+acf = plot_acf(sp_500, lags = 20)
 acf.savefig("timeSeriesACF.png", format = 'png')
 
 
 
-pacf = plot_pacf(ts, lags = 20)
+pacf = plot_pacf(sp_500, lags = 20)
 pacf.savefig("timeSeriesPACFDiff.png", format = 'png')
 
 
-acfDiff = plot_acf(ts_diff, lags = 20)
+acfDiff = plot_acf(diff, lags = 20)
 acfDiff.savefig("timeSeriesACFDiff.png", format = 'png')
 
 
-pacfDiff = plot_pacf(ts_diff, lags = 20)
+pacfDiff = plot_pacf(diff, lags = 20)
 pacfDiff.savefig("timeSeriesPACFDiff.png", format = 'png')
 
 
@@ -129,14 +127,14 @@ pacfDiff.savefig("timeSeriesPACFDiff.png", format = 'png')
 ####
 ##
 
-model = ARIMA(ts, order = (0, 1, 1))
+model = ARIMA(sp_500, order = (0, 1, 1))
 fit = model.fit(disp=-1)
 
 fit = pd.Series(fit.fittedvalues, copy = True)
 fit= fit.cumsum()
 fit = fit
 
-plt.plot(ts)
+plt.plot(sp_500)
 plt.plot(fit)
 plt.title('Fitted Vs. Actual Values for ')
 plt.savefig("realVPred.png", format = 'png')
